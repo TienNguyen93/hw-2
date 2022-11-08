664d6ff909ed23035eb81449915e193cd269dd95
TienNguyen93 <nguyentien9933@gmail.com>
2022-11-07 23:45:27 -0500

    create 8 faced down cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './component/Card';
 
 function App() {
+  const cardValues = [
+    "Card1", "Card2", "Card3", "Card4",
+    "Card5", "Card6", "Card7", "Card8"
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='card-list'>
+        {cardValues.map((card, idx) => (
+          <Card key={idx} cardValue={card} />
+        ))}
+      </div>
+
     </div>
   );
 }
