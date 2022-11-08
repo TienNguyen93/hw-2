b8111a39387e1b8be56e8188121ea12c61e7a95f
TienNguyen93 <nguyentien9933@gmail.com>
2022-11-08 00:06:05 -0500

    change card's color & state after click

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 const Card = (props) => {
+  const [isFlipped, setIsFlipped] = useState(false)
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped)
+    console.log(isFlipped)
+  }
+
+  if (isFlipped === false) {
+    return (
+      <div className="card" onClick={handleClick}>
+        {props.cardValue}
+      </div>
+    )
+  }
+
   return (
-    <div className="card">
+    <div className="card-flip" onClick={handleClick}>
       {props.cardValue}
     </div>
   )
+
 }
 
 export default Card
