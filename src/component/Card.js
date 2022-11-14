fcc6d2e27ab3f062b7ca0c0cd0e2133092b6d0af
TienNguyen93 <nguyentien9933@gmail.com>
2022-11-13 20:54:42 -0500

    add styling to button & cards

--- src/component/Card.js
+++ src/component/Card.js
@@ -5,11 +5,27 @@ const Card = ({ card, index, handleCardClick, isCardChosen}) => {
     handleCardClick(card.type, index)
   }
 
+  const style = {
+    card: {
+      height: "100px",
+      width: "90px",
+      backgroundColor: "#F5F5DC",
+      color: "blue",
+      cursor: "pointer",
+      position: "relative",
+      borderRadius: "15px"
+    }
+  }
+
   return (
-    <div className="card" onClick={handleClick}>
+    <div style={style.card} onClick={handleClick}>
       {isCardChosen(card.type, index) 
-        ? <b>{card.type}</b> 
-        : <b>not chosen</b>}
+        ? 
+        <div style={{...style.card, border: '1.5px solid black'}}>
+          <h3>{card.type}</h3>
+        </div> 
+        : <div style={{...style.card, 
+          backgroundColor: '#C576F6'}}></div> }
     </div>
   )
 }
